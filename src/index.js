4b50cc867d7ee4e286b4716f76f00a0a93afc790
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-20 23:10:52 +0200

    feat: implemented human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    var ones = new Array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen');
+    var tens = new Array('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety');
+    let result = '';
+    var one = number % 10;
+    var ten = Math.floor(number / 10) % 10;
+    var hunder = Math.floor(number / 100);
+
+    if (hunder != 0) {
+        result = result + ones[hunder] + ' hundred';
+    }
+    if (ten != 0) {
+        if (result != '') {
+            result = result + ' ';
+        }
+        if (ten === 1) {
+            result = result + ones[number - hunder * 100];
+        } else {
+            result = result + tens[ten];
+        }
+    }
+    if (one != 0 && ten != 1) {
+        if (result != '') {
+            result = result + ' ';
+        }
+        result = result + ones[one];
+    } else if (one === 0 && result === '') {
+        result = ones[one];
+    }
+    return result;
 }
